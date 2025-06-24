33b3c593a91530adcf680ef83f75611294f16dd5
Scott Oller <oller2.scott@gmail.com>
2025-06-24 08:31:48 +0000

    im getting this error in my googtle cloud console whe its builkding Expo

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,6 +18,8 @@ import { useToast } from '@/hooks/use-toast';
 import { Loader2 } from 'lucide-react';
 import Image from 'next/image';
 
+export const dynamic = 'force-dynamic';
+
 const loginFormSchema = z.object({
   email: z.string().email({ message: 'Please enter a valid email address.' }),
   password: z.string().min(1, { message: 'Password is required.' }),
