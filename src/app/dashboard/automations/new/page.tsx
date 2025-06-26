a6b2faf219d56ccc656e7857e751afd2ede13e0b
Scott Oller <oller2.scott@gmail.com>
2025-06-26 06:28:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/automations/new/page.tsx
+++
@@ -1,5 +0,0 @@
-// This file is obsolete and will be removed. The functionality is now in [automationId]/page.tsx.
-// To prevent build errors, this file is left with a placeholder.
-export default function ObsoleteNewAutomationPage() {
-    return null;
-}
